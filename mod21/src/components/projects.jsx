848a5c9a59277148cc6244d047a27648a703fbab
Rubenvill0811 <rubenvill0811@gmail.com>
2024-07-02 18:35:18 -0700

    added some css styles

---
+++ mod21/src/components/projects.jsx
@@ -0,0 +1,31 @@
+import './Projects.css'
+
+const ProjectGrid = () => {
+
+    return(
+        <div className='bod'>
+            <div className="container">
+            <h1>hi</h1>
+            <div className="col-left">
+                <div className="row row1">
+                   
+                </div>
+                <div className="row row2"></div>
+                <div className="row row3">
+                    <div className="row-col left"></div>
+                    <div className="row-col right"></div>
+                </div>
+            </div>
+            <div className="col-right">
+                <div className="row top"></div>
+                <div className="row bottom"></div>
+            </div>
+            </div>
+        </div>
+    );
+
+
+
+}
+
+export default ProjectGrid;
