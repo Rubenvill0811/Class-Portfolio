b466964370b33fffa322b9f24369f74a80a9e0eb
Rubenvill0811 <rubenvill0811@gmail.com>
2024-07-03 22:04:30 -0700

    added some filler text

--- mod21/src/components/projects.jsx
+++ mod21/src/components/projects.jsx
@@ -8,17 +8,29 @@ const ProjectGrid = () => {
             <h1>hi</h1>
             <div className="col-left">
                 <div className="row row1">
-                   
+                   <div className="me">
+                    <p>This is my  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Harum eligendi quos nemo vel eum iste adipisci repudiandae perferendis recusandae, pariatur explicabo quas in, ratione doloribus? Est dignissimos rem rerum atque!</p>
+                   </div>
                 </div>
-                <div className="row row2"></div>
+                <div className="row row2 row2text">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat dolorem natus minus nobis cupiditate in ea. Cumque, nostrum culpa minus natus modi laudantium quibusdam tempora pariatur eum blanditiis ab officia.</p>
+                </div>
                 <div className="row row3">
-                    <div className="row-col left"></div>
-                    <div className="row-col right"></div>
+                    <div className="row-col left">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus quia ducimus et quos molestiae esse est eos maiores. Numquam eveniet at ipsum distinctio, error quos officia nemo voluptatum. Laboriosam, ullam.</p>
+                    </div>
+                    <div className="row-col right">
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Incidunt architecto ab quos sed rerum at. Voluptate illo et laborum corrupti, ipsum recusandae temporibus maxime hic, facilis repudiandae vero delectus sed.</p>
+                    </div>
                 </div>
             </div>
             <div className="col-right">
-                <div className="row top"></div>
-                <div className="row bottom"></div>
+                <div className="row top">
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. In laboriosam quae exercitationem quasi iste nulla consequatur laudantium quis quisquam perspiciatis iure enim alias animi dolores quaerat, assumenda, aperiam, omnis placeat.</p>
+                </div>
+                <div className="row bottom">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In nisi magni repudiandae pariatur vero corporis ipsa quae assumenda, consequatur tempora similique ut excepturi nam consequuntur provident eligendi natus exercitationem. A.</p>
+                </div>
             </div>
             </div>
         </div>
